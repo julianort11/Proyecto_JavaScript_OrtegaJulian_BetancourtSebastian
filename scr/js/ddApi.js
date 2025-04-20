ae4cf4a4d738687030116d2ac890c8bbd521a0db
julianort11 <ort.na140@gmail.com>
2025-04-20 15:21:09 -0500

    feat: :sparkles: consumption of the API

---
+++ scr/js/ddApi.js
@@ -0,0 +1,21 @@
+const BASE_URL = "https://www.dnd5eapi.co/api";
+
+export function fetchClasses() {
+  return fetch(`${BASE_URL}/classes`).then(respu => respu.json());
+}
+
+export function fetchRaces() {
+  return fetch(`${BASE_URL}/races`).then(respu => respu.json());
+}
+
+export function fetchArmors() {
+  return fetch(`${BASE_URL}/equipment-categories/armor`).then(respu => respu.json());
+}
+
+export function fetchWeapons() {
+  return fetch(`${BASE_URL}/equipment-categories/weapon`).then(respu => respu.json());
+}
+
+export function fetchFeatures() {
+  return fetch(`${BASE_URL}/traits`).then(respu => respu.json());
+}
