32a36fa9972ebc1722e4ebbbedc8f84cddb7cc8c
julianort11 <ort.na140@gmail.com>
2025-04-22 00:41:41 -0500

    feat: :construction: in process section contac us

--- scr/js/game.js
+++ scr/js/game.js
@@ -1,14 +1,14 @@
 import { bookshop, loadedBookshop } from "./library.js";
-import {renderCharacters} from "./characters.js";
+import { renderCharacters } from "./characters.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const mainContent = document.getElementById("mainContent");
 
-    document.querySelector('a[href="#Character"]').addEventListener("click", (e) => {
-        e.preventDefault();
-        renderCharacters();
-    });
+  document.querySelector('a[href="#Character"]').addEventListener("click", (e) => {
+    e.preventDefault();
+    renderCharacters();
+  });
 
   document.querySelector('a[href="#Library"]').addEventListener("click", (e) => {
     e.preventDefault();
@@ -22,6 +22,18 @@ document.addEventListener("DOMContentLoaded", () => {
     mainContent.innerHTML = `
         <h2>Contacto</h2>
         <p>Para dudas</p>
+        <h3>informacion:</h3>
+        <div class="div__name">
+          <h4>Jose Julian Ortega Navarro</h4>
+            <div class="div__name_img">
+                <div class="backgr__img">
+                   <a href=""><img src="/assets/img/klipartz.com.png"></a>
+                </div>
+                <div>
+                   <a href=""><img src="/assets/img/logig.png"></a>
+                </div>
+            </div>
+        </div>
       `;
   });
 });
