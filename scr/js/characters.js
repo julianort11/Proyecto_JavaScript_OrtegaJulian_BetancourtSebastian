a920020af46836c6485eb5288f526c6fee89086e
julianort11 <ort.na140@gmail.com>
2025-04-24 05:55:44 -0500

    feat: :lipstick: new styles for the create chearts

--- scr/js/characters.js
+++ scr/js/characters.js
@@ -12,94 +12,94 @@ const characters = async () => {
 
         let charactersHTML = `
         <div class="main__characters">
-          <h2>Characters</h2>
-          <p>Create your character and get ready for the adventure</p>
-        </div>
-            <dialog id="characterDialog" class="dialog__Add">
-              <div class="dialog__container">
-                <button class="button__close" id="closeDialog">X</button>
-                <div class="dialog__header">
-                  <h2>Create your character</h2>
-                </div>
-                <div class="dialog__content">
-                  <!-- Campo de nombre -->
-                  <div class="div__form">
-                    <form id="characterForm">
+        <h2>Characters</h2>
+        <p>Create your character and get ready for the adventure</p>
+      </div>
+          <dialog id="characterDialog" class="dialog__Add">
+            <div class="dialog__container">
+              <button class="button__close" id="closeDialog">X</button>
+              <div class="dialog__header">
+                <h2>Create your character</h2>
+              </div>
+              <div class="dialog__content">
+                <!-- Campo de nombre -->
+                <div class="div__form">
+                  <form id="characterForm">
+                    <div class="div__write">
                       <span>Write name of character: </span>
                       <input type="text" name="name" id="name" placeholder="Enter character name" required />
                       <span>Write detail of character: </span>
                       <input type="text" name="details" id="details" placeholder="Enter character details" />
-                    
-                  </div>
-
-                  <!-- Carrusel de clases -->
-                  <div class="classGame">
-                    <strong>Choose your Class:</strong>
-                    <div class="carousel">
-                      <div class="carousel-inner" id="classCarousel">
-                        <!-- Clases se cargarán dinámicamente aquí -->
-                      </div>
-                      <button class="carousel-control prev" id="prevButton">❮</button>
-                      <button class="carousel-control next" id="nextButton">❯</button>
                     </div>
-                  </div>
-
-                  <!-- Selector de raza -->
-                  <div class="raceGame">
-                    <strong>Choose your Race:</strong>
-                    <select id="raceSelect" class="select-group"></select>
-                  </div>
-
-                  <!-- Selector de género -->
-                  <div class="genderGame">
-                    <strong>Choose your Gender:</strong>
-                    <select id="genderSelect">
-                      <option value="Male">Masculino</option>
-                      <option value="Female">Femenino</option>
-                      <option value="Other">Otro</option>
-                    </select>
-                  </div>
-
-                  <!-- Equipo y armas -->
-                  <div class="armorWeapons">
-                    <strong>Choose your Armor and Weapons:</strong>
-
-                    <select id="equipmentSelect"></select>
-                  </div>
-
-                  <!-- Habilidades -->
-                  <div class="abilities">
-                    <strong>Abilities:</strong>
-                    <select id="abilities"></select>
-                  </div>
-
-                  <!-- Habilidades especiales -->
-                  <div class="specialSkills">
-                    <strong>Special Skills:</strong>
-                    <select id="skills"></select>
-                  </div>
-
-                  <!-- Accesorios -->
-                  <div class="accessories">
-                    <strong>Accessories:</strong>
-                    <select id="accessories"></select>
-                  </div>
-
-                  <!-- Botón de guardar -->
-                  <input type="submit" id="saveCharacter" value="Save Character">
-                </form>
-                  </div>
-              </div>
-            </dialog>
-
-            <div class="div__characters">
-                <div class="div__add">
-                    <button class="button__add" id="openDialog">
-                        <div class="button__add">
-                            <img src="../../assets/img/Add.svg" alt="Add Character">
-                        </div>
-                    </button>
                 </div>
+
+                <!-- Carrusel de clases -->
+                <div class="classGame">
+                  <strong>Choose your Class:</strong>
+                  <div class="carousel">
+                    <div class="carousel-inner" id="classCarousel">
+                      <!-- Clases se cargarán dinámicamente aquí -->
+                    </div>
+                    <button class="carousel-control prev" id="prevButton">❮</button>
+                    <button class="carousel-control next" id="nextButton">❯</button>
+                  </div>
+                </div>
+
+                <!-- Selector de raza -->
+                <div class="raceGame">
+                  <strong>Choose your Race:</strong>
+                  <select id="raceSelect" class="select-group"></select>
+                </div>
+
+                <!-- Selector de género -->
+                <div class="genderGame">
+                  <strong>Choose your Gender:</strong>
+                  <select id="genderSelect">
+                    <option value="Male">Masculino</option>
+                    <option value="Female">Femenino</option>
+                    <option value="Other">Otro</option>
+                  </select>
+                </div>
+
+                <!-- Equipo y armas -->
+                <div class="armorWeapons">
+                  <strong>Choose your Armor and Weapons:</strong>
+                  <select id="equipmentSelect"></select>
+                </div>
+
+                <!-- Habilidades -->
+                <div class="abilities">
+                  <strong>skills:</strong>
+                  <select id="abilities"></select>
+                </div>
+
+                <!-- Habilidades especiales -->
+                <div class="specialSkills">
+                  <strong>Special Skills:</strong>
+                  <select id="skills"></select>
+                </div>
+
+                <!-- Accesorios -->
+                <div class="accessories">
+                  <strong>Accessories:</strong>
+                  <select id="accessories"></select>
+                </div>
+
+                <!-- Botón de guardar -->
+                <input class="send" type="submit" id="saveCharacter" value="Save Character">
+              </form>
+                </div>
+            </div>
+          </dialog>
+
+          <div class="div__characters">
+              <div class="div__add">
+                  <button class="button__add" id="openDialog">
+                      <div class="button__add">
+                          <img src="../../assets/img/Add.svg" alt="Add Character">
+                      </div>
+                  </button>
+              </div>
         `;
 
         for (const character of data.results) {
