183c9c42a3906150abca6e7c5ef0a7aeea31842b
julianort11 <ort.na140@gmail.com>
2025-04-24 21:29:08 -0500

    feat: :lipstick: new design to view your characters

--- scr/js/main.js
+++ scr/js/main.js
@@ -1,15 +1,5 @@
 import { initTransition } from './transition.js';
 
-const video = document.getElementById("video__background");
-const btnSonido = document.getElementById("actSond");
-
-btnSonido.addEventListener("click", () => {
-  video.muted = false;
-  video.volume = 1;
-  video.play();
-  btnSonido.style.display = "none";
-});
-
 document.addEventListener('DOMContentLoaded', () => {
   initTransition('.button__game'); 
 });
